9ce0487093c44f65b949fd27cc3652be4d4a34e6
mtbrancato <mtbrancato1189@gmail.com>
2017-08-28 23:25:00 -0700

    my second commit

---
+++ src/com/company/VendingMachine.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class VendingMachine {
+
+    public VendingMachine(){
+
+    }
+
+    public String AddItem()
+}
+
+public class Item {
+
+    public Item(String type, int num){
+        Name = type;
+        Quantity = num;
+    }
+}
